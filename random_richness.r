84204250a26a0858a15393ff76033c29b7de2b6c
Fabricio Villalobos <fabro@Mac-d-Fab.local>
2014-02-28 10:39:58 -0300

    randomrichness

---
+++ random_richness.r
@@ -0,0 +1,27 @@
+## Carlos López-Natarén (@natorro) & Fabricio Villalobos (@fabro)
+
+## Function: generate n (n = simulations of size 'size_vector') random ranges
+## arguments as in rangeMap and rangePam
+
+
+
+randomRichness<-function(data_file, vector_of_range_sizes, sims){
+
+  for (i in 1:sims){
+  
+  simranges <- length (vector_of_range_sizes)
+  
+  random_richness_matrix <- matrix (0, nrow=175, ncol=175)
+  
+    for (j in 1:simranges) {                    
+
+    temporary_matrix <- randomMap(data_file,vector_of_range_sizes[j])
+    random_richness_matrix <- random_richness_matrix + temporary_matrix
+    temporary_matrix <- matrix (0, nrow=175, ncol=175)
+    }
+  
+  write.table(random_richness_matrix, paste('random_matrix_', i, '.txt', sep=""), sep="\t") 
+  
+  }
+  
+}
